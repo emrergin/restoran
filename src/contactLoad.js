f5123a22c460856ec4020aa2f606e1bda534862e
emrergin <emrergin2757@yahoo.com>
2022-01-10 01:43:40 +0300

    arranged layout for the menu

--- src/contactLoad.js
+++ src/contactLoad.js
@@ -1,12 +1,16 @@
 function contactLoad(){
   const bilgiBaslik=document.createElement(`h1`);
   bilgiBaslik.textContent=`Neredeyiz?`;
-  const Harita=document.createElement(`div`);  
-  Harita.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3058.954648447218!2d33.39680595083063!3d39.94240409217298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf7387337fb908ab1!2zMznCsDU2JzMyLjYiTiAzM8KwMjMnNTYuNCJF!5e0!3m2!1str!2str!4v1641134803765!5m2!1str!2str" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`;
-  
+  // bilgiBaslik
+  const Harita=document.createElement(`iframe`);
+  Harita.setAttribute(`src`,"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3058.954648447218!2d33.39680595083063!3d39.94240409217298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf7387337fb908ab1!2zMznCsDU2JzMyLjYiTiAzM8KwMjMnNTYuNCJF!5e0!3m2!1str!2str!4v1641134803765!5m2!1str!2str");
+  Harita.setAttribute(`allowfullscreen`,"");
+  Harita.setAttribute(`loading`,"lazy");
+  Harita.classList.add(`harita`);
 
 
   const bilgiGenel=document.createElement('div');
+  bilgiGenel.classList.add(`orta3`);
   bilgiGenel.appendChild(bilgiBaslik);
   bilgiGenel.appendChild(Harita);
   return bilgiGenel;
--- src/menuLoad.js
+++ src/menuLoad.js
@@ -1,6 +1,54 @@
 function menuLoad(){
+  const card1=document.createElement(`div`); 
+  const card2=document.createElement(`div`);
+  const card3=document.createElement(`div`)
+  const card4=document.createElement(`div`);
+  card1.classList.add(`menuitem`);
+  card2.classList.add(`menuitem`);
+  card3.classList.add(`menuitem`);
+  card4.classList.add(`menuitem`);
+
+
+
+  const menu=document.createElement(`div`);
+  menu.classList.add(`menuAlbum`);
+
+  const resim1= new Image();
+  const resim2= new Image();
+  const resim3= new Image();
+  const resim4= new Image();
+  resim1.src=`https://via.placeholder.com/150`;
+  resim2.src=`https://via.placeholder.com/150`;
+  resim3.src=`https://via.placeholder.com/150`;
+  resim4.src=`https://via.placeholder.com/150`;
+
+  const yazi1=document.createElement(`span`);
+  const yazi2=document.createElement(`span`);
+  const yazi3=document.createElement(`span`);
+  const yazi4=document.createElement(`span`);
+  yazi1.textContent="Eh İşte";
+  yazi2.textContent="Eh İşte";
+  yazi3.textContent="Eh İşte";
+  yazi4.textContent="Eh İşte";
+
+
+  card1.appendChild(resim1);
+  card2.appendChild(resim2);
+  card3.appendChild(resim3);
+  card4.appendChild(resim4);
+  card1.appendChild(yazi1);
+  card2.appendChild(yazi2);
+  card3.appendChild(yazi3);
+  card4.appendChild(yazi4);
+
+
   const menuGenel=document.createElement('div');
-  menuGenel.textContent=`MENU DENEME`;
+  menu.appendChild(card1);
+  menu.appendChild(card2);
+  menu.appendChild(card3);
+  menu.appendChild(card4);
+  menuGenel.appendChild(menu);
+  menuGenel.setAttribute(`id`,`menuContainer`);
   
   return menuGenel;
 
